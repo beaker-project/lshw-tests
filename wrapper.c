7fe2dafb405e8e0df9481f758258091f886f4d43
Dan Callaghan <dcallagh@redhat.com>
2015-06-29 13:57:01 +1000

    wrapper.c: ioctl redirection for block devices

--- wrapper.c
+++ wrapper.c
@@ -4,11 +4,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/utsname.h>
+#include <sys/ioctl.h>
+#include <linux/fs.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <glob.h>
@@ -51,7 +55,8 @@ static const char *get_test_dir(void) {
     static char *d = NULL;
     if (d == NULL) {
         d = getenv("LSHW_TEST_DIR");
-        if (d == NULL) return NULL;
+        if (d == NULL || d[0] == '\0')
+            die("LSHW_TEST_DIR environment variable not set");
         // convert to absolute path, makes things easier below
         d = realpath(d, NULL);
         // strip trailing slash if any
@@ -63,8 +68,6 @@ static const char *get_test_dir(void) {
 
 static void strip_test_dir_inplace(char *pathname) {
     const char *prefix = get_test_dir();
-    if (prefix == NULL)
-        return;
     size_t prefix_len = strlen(prefix);
     if (strncmp(pathname, prefix, prefix_len) == 0) {
         size_t i;
@@ -77,8 +80,6 @@ static void strip_test_dir_inplace(char *pathname) {
 
 static char *redirected_path(const char *pathname) {
     const char *test_dir = get_test_dir();
-    if (test_dir == NULL || test_dir[0] == '\0')
-        return NULL;
     char *abs;
     if (pathname[0] != '/') {
         // relative path, make it absolute
@@ -104,8 +105,6 @@ static char *redirected_path(const char *pathname) {
 
 static char *redirected_sysconf_path(int name) {
     const char *test_dir = get_test_dir();
-    if (test_dir == NULL || test_dir[0] == '\0')
-        return NULL;
     char *path = malloc(strlen(test_dir) + 12);
     sprintf(path, "%s/sysconf/%d", test_dir, name);
     return path;
@@ -113,11 +112,57 @@ static char *redirected_sysconf_path(int name) {
 
 static char *redirected_arch_path(void) {
     const char *test_dir = get_test_dir();
-    if (test_dir == NULL || test_dir[0] == '\0')
-        return NULL;
     char *path = malloc(strlen(test_dir) + 6);
     sprintf(path, "%s/arch", test_dir);
     return path;
+}
+
+// For now, assumes block devices!
+static char *redirected_sysfs_attr_path(const char *dev, const char *attr) {
+    const char *test_dir = get_test_dir();
+    char *path = malloc(strlen(test_dir) + strlen(dev) + strlen(attr) + 13);
+    sprintf(path, "%s/sys/block/%s/%s", test_dir, basename(dev), attr);
+    return path;
+}
+
+static int64_t load_int(const char *path) {
+    FILE *f = fopen(path, "r");
+    if (f == NULL)
+        die("failed to open %s", path);
+    int64_t result;
+    if (fscanf(f, "%" PRIi64, &result) != 1)
+        die("failed to read integer value from %s", path);
+    fclose(f);
+    return result;
+}
+
+static uint64_t load_uint(const char *path) {
+    FILE *f = fopen(path, "r");
+    if (f == NULL)
+        die("failed to open %s", path);
+    uint64_t result;
+    if (fscanf(f, "%" PRIu64, &result) != 1)
+        die("failed to read integer value from %s", path);
+    fclose(f);
+    return result;
+}
+
+static uint64_t load_sysfs_attr_uint(const char *dev, const char *attr) {
+    char *p = redirected_sysfs_attr_path(dev, attr);
+    uint64_t result = load_uint(p);
+    free(p);
+    return result;
+}
+
+static char *path_for_fd(int fd) {
+    char proc_pathname[22];
+    sprintf(proc_pathname, "/proc/self/fd/%d", fd);
+    char *fd_pathname = (char *)malloc(2048);
+    ssize_t fd_pathname_len = readlink(proc_pathname, fd_pathname, 2048);
+    if (fd_pathname_len < 0 || fd_pathname_len >= 2048)
+        die("failed to read symlink %s", proc_pathname);
+    fd_pathname[fd_pathname_len] = '\0';
+    return fd_pathname;
 }
 
 int chdir(const char *path) {
@@ -235,6 +280,58 @@ FILE *fopen(const char *path, const char *mode) {
     }
 }
 
+int ioctl(int d, unsigned long request, ...) {
+    va_list args;
+    va_start(args, request);
+    void *argp = va_arg(args, void *);
+    va_end(args);
+    static long (*real_func)(int, int, void *);
+    if (real_func == NULL) {
+        real_func = dlsym(RTLD_NEXT, "ioctl");
+        if (real_func == NULL)
+            die("failed to load real 'ioctl': %s", dlerror());
+    }
+    if (d <= 0)
+        goto passthrough;
+    char *fd_pathname = path_for_fd(d);
+    // is the fd for a redirected path?
+    const char *test_dir = get_test_dir();
+    if (strncmp(fd_pathname, test_dir, strlen(test_dir)) != 0) {
+        free(fd_pathname);
+        goto passthrough;
+    }
+    switch (request) {
+        case BLKGETSIZE64: {
+            uint64_t sectors = load_sysfs_attr_uint(fd_pathname, "size");
+            // sysfs size is always in 512-byte "UNIX sectors" (I think...)
+            uint64_t size = sectors * 512;
+            info("ioctl() returning BLKGETSIZE64 value %" PRIu64 " for %s",
+                    size, fd_pathname);
+            *((uint64_t *)argp) = size;
+        } break;
+        case BLKPBSZGET: {
+            unsigned int phys_block_size = (unsigned int) load_sysfs_attr_uint(
+                    fd_pathname, "queue/physical_block_size");
+            info("ioctl() returning BLKPBSZGET value %u for %s",
+                    phys_block_size, fd_pathname);
+            *((unsigned int *)argp) = phys_block_size;
+        } break;
+        case BLKSSZGET: {
+            int logical_block_size = (int) load_sysfs_attr_uint(fd_pathname,
+                    "queue/logical_block_size");
+            info("ioctl() returning BLKSSZGET value %d for %s",
+                    logical_block_size, fd_pathname);
+            *((int *)argp) = logical_block_size;
+        } break;
+        default:
+            die("unimplemented ioctl() %lu", request);
+    }
+    free(fd_pathname);
+    return 0;
+passthrough:
+    return real_func(d, request, argp);
+}
+
 long sysconf(int name) {
     static long (*real_func)(int);
     if (real_func == NULL) {
@@ -244,16 +341,10 @@ long sysconf(int name) {
     }
     char *p = redirected_sysconf_path(name);
     if (p != NULL) {
-        FILE *f = fopen(p, "r");
-        if (f == NULL)
-            die("failed to fopen %s", p);
-        long value;
-        if (fscanf(f, "%ld", &value) != 1)
-            die("failed to read value from %s", p);
-        fclose(f);
+        int64_t value = load_int(p);
         info("sysconf() returning value %ld from %s", value, p);
         free(p);
-        return value;
+        return (long) value;
     } else {
         return real_func(name);
     }
